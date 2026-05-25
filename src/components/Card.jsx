a52eadc0d52ee740ad39d25c829f413ff9866059
tired_ <omaralim06@gmail.com>
2026-05-25 02:31:58 -0700

    feat: implement FontAwesome icons & implement icon interactivity in Cards.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router'
 import styles from './Card.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink, faCircleInfo, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function Card( {name, url, description, imageURL}) {
     const bgStyle = {
         backgroundImage: `url(${imageURL})`
     };
+
+    const buttonStyle = {
+        color: `white`
+    };
+
+
 
     return (
         <div className={styles.container}>
@@ -16,15 +24,21 @@ export default function Card( {name, url, description, imageURL}) {
                     </div>
                     <div className={styles.buttons}>
                         <div className={`${styles.button} ${styles['view-creator']}`}>
-                            <Link to='/view' state={{ creator: { name, url, description, imageURL } }}>ℹ️</Link>
+                            <Link to='/view' state={{ creator: { name, url, description, imageURL } }}>
+                                <FontAwesomeIcon icon={faCircleInfo} />
+                            </Link>
                         </div>
                         <div className={`${styles.button} ${styles['edit-creator']}`}>
-                            <Link to='/edit' state={{ creator: { name, url, description, imageURL } }}>📝</Link>
+                            <Link to='/edit' state={{ creator: { name, url, description, imageURL } }}>
+                                <FontAwesomeIcon icon={faPenToSquare} />
+                            </Link>
                         </div>
                     </div>
                 </div>
                 <div className={styles.url}>
-                    <a target='_blank' href={url}>🔗</a>
+                    <a target='_blank' href={url}>
+                        <FontAwesomeIcon icon={faLink} style={buttonStyle} />
+                    </a>
                 </div>
                 <div className={styles.description}>
                     {description}

e04903eb8eef2fd5810681b67faa5778e078e84f
tired_ <omaralim06@gmail.com>
2026-05-25 01:17:05 -0700

    refactor: migrate from standard CSS to CSS Modules-type styling for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { Link } from "react-router";
-import './App.css'
+import styles from './App.module.css'
 import Button from './components/Button.jsx'
 
 export default function App() {
   return (
     <>
       <h1>Creatorverse</h1>
-      <div className='navlinks'>
+      <div className={styles.navlinks}>
         <Button name='Show Creators' link='creators'></Button>
       </div>
     </>
